4da058abe07188ec2c665af5492ddcee871ad515
charles <chuckhperry@gmail.com>
2020-08-09 19:44:03 -0700

    Added some to the about page

--- pages/about.js
+++ pages/about.js
@@ -4,8 +4,25 @@ export default function About() {
     return (
         <div>
             <Navbar/>
-            <div>
+            <div className={styles.description}>
                 <h1>Hi I'm Charles</h1>
+                <h3>Aspiring Dev who loves the functional side of the development process.</h3>
+                <div>
+                    <p>Technologies I've used:</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>Postgres</li>
+                        <li>Mongo</li>
+                        <li>Sql</li>
+                        <li>React</li>
+                        <li>Next.js</li>
+                        <li>Node.js</li>
+                        <li>TypeScript</li>
+                        <li>Express</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
