5079a1cc60d8d26391870c30cf4be77bde5adf01
charles <chuckhperry@gmail.com>
2020-10-14 16:43:25 -0700

    Local server was non-functional for a few hpurs while trying to debug gsap module intergration, now working again

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/header'
 import Carousel from 'react-bootstrap/Carousel'
 import { useState, useEffect, useRef } from 'react'
 import dynamic from 'next/dynamic'
-import { gsap, TweenMax, Power3 } from 'gsap'
+import { TweenMax, Power3} from 'gsap/index'
 
 
 let carouselText = [
